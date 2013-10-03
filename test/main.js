810e8b151bf0cede3c1dd350bdb57afec86e7252
Nicolas Grenié <nicolas.grenie@gmail.com>
2013-10-03 13:22:53 +0200

    change on package + test

--- test/main.js
+++ test/main.js
@@ -1,11 +1,11 @@
 var should = require('should');
 var sleepsort = require('../lib/main');
 
-describe('sleepsort', function() {
-    describe('with no arguments', function() {
-        it('returns an empty array', function() {
-            var result = sleepsort();
+describe('with an empty array argument', function() {
+    it('calls the callback with an empty array', function(done) {
+        var result = sleepsort([], function(result) {
             result.should.eql([]);
+            done();
         });
     });
 });
