2768318f9548a0d078592dff614d92eceed40716
Nicolas Grenié <nicolas.grenie@gmail.com>
2013-10-03 13:34:39 +0200

    new test

--- test/main.js
+++ test/main.js
@@ -9,3 +9,21 @@ describe('with an empty array argument', function() {
         });
     });
 });
+
+describe('with a single element array', function() {
+    it('calls the callback with a single element array', function(done) {
+        var result = sleepsort([1], function(result) {
+            result.should.eql([1]);
+            done();
+        });
+    });
+});
+
+describe('with an unsorted two element array', function() {
+    it('calls the callback with a sorted two element array', function(done) {
+        var result = sleepsort([2, 1], function(result) {
+            result.should.eql([1, 2]);
+            done();
+        });
+    });
+});
